3179535f4768da583dc01ad0d45f98fc1022055e
Alroy <alroypereira05@gmail.com>
2025-03-31 19:53:03 +0530

    auth implemented

---
+++ backend/src/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+import { connectDB } from "./lib/db.js";
+import authRoutes from './routes/auth.route.js';
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors(
+  {
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }
+));
+app.use(cookieParser());
+
+app.use('/api/auth', authRoutes);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+})
